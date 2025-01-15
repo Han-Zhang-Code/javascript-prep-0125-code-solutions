1b805381cd8c4656574cc16e36856731b38271ff
Han-Zhang-Code <zyh514312@gmail.com>
2025-01-15 04:38:59 +0000

    done

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,34 @@
+const number1 = 1;
+const number2 = 2;
+const product = number1 * number2;
+console.log('the value of product: ', product);
+console.log('the type of the product: ', typeof product);
+
+const charge = 2;
+const payment = 1;
+const amountRemaining = charge * payment;
+console.log('the value of amountRemaining: ', amountRemaining);
+console.log('the type of the amountRemaining: ', typeof amountRemaining);
+
+const tests = 2;
+const assignments = 1;
+const final = 3;
+const grade = (tests + assignments + final) / 3;
+console.log('the value of grade: ', grade);
+console.log('the type of the grade: ', typeof grade);
+
+const firstName = 'Yuhan';
+const lastName = 'Zhang';
+const fullName = `${firstName} ${lastName}`;
+console.log('the value of fullName: ', fullName);
+console.log('the type of the fullName: ', typeof fullName);
+
+const pH = 7;
+const isAcidic = pH < 7;
+console.log('the value of isAcidic: ', isAcidic);
+console.log('the type of the isAcidic: ', typeof isAcidic);
+
+let nickname = fullName;
+nickname += ' is the GOAT';
+console.log('the value of nickname: ', nickname);
+console.log('the type of the nickname: ', typeof nickname);
