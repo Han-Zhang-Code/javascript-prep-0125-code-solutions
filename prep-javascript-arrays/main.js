4c7b95bfe076ebcb2a70f3f91977638f8d106e1e
Han-Zhang-Code <zyh514312@gmail.com>
2025-01-17 03:51:10 +0000

    done

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,11 @@
+const languages = ['html', 'css', 'javaScript', 'react', 'node', 'postgres'];
+console.log('the value of languages is :', languages);
+
+const firstElement = languages[0];
+console.log('the value of firstElement is :', firstElement);
+const thirdElement = languages[2];
+console.log('the value of thirdElement is :', thirdElement);
+const length = languages.length;
+console.log('the value of length is :', length);
+const lastElement = languages[length - 1];
+console.log('the value of lastElement is :', lastElement);
